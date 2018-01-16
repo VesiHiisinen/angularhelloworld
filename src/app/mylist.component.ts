40bd9563acccea8fea8f2e45e533eca387842b2f
VilleVettenranta <ville.vettenranta@gmail.com>
2018-01-16 10:38:54 +0200

    first draft angular project

---
+++ src/app/mylist.component.ts
@@ -0,0 +1,411 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:"my-list",
+    templateUrl:"mylist.component.html"
+})
+
+export class MyComponent {
+    dataList = [
+	{
+		"firstName": "Zephania",
+		"lastName": "Whitney"
+	},
+	{
+		"firstName": "Lacota",
+		"lastName": "Wolfe"
+	},
+	{
+		"firstName": "Rana",
+		"lastName": "Blackburn"
+	},
+	{
+		"firstName": "Fleur",
+		"lastName": "Emerson"
+	},
+	{
+		"firstName": "Dacey",
+		"lastName": "Riley"
+	},
+	{
+		"firstName": "Emily",
+		"lastName": "Hendricks"
+	},
+	{
+		"firstName": "Lucian",
+		"lastName": "Ray"
+	},
+	{
+		"firstName": "Fuller",
+		"lastName": "Gilbert"
+	},
+	{
+		"firstName": "Stella",
+		"lastName": "Franklin"
+	},
+	{
+		"firstName": "Arsenio",
+		"lastName": "Avery"
+	},
+	{
+		"firstName": "Jena",
+		"lastName": "Carpenter"
+	},
+	{
+		"firstName": "Drake",
+		"lastName": "Ochoa"
+	},
+	{
+		"firstName": "Ferris",
+		"lastName": "Mcfarland"
+	},
+	{
+		"firstName": "Nicholas",
+		"lastName": "Richardson"
+	},
+	{
+		"firstName": "Hamilton",
+		"lastName": "Mejia"
+	},
+	{
+		"firstName": "Ross",
+		"lastName": "Oneill"
+	},
+	{
+		"firstName": "Jenette",
+		"lastName": "Kennedy"
+	},
+	{
+		"firstName": "Garrison",
+		"lastName": "Campos"
+	},
+	{
+		"firstName": "Aaron",
+		"lastName": "Weeks"
+	},
+	{
+		"firstName": "Amelia",
+		"lastName": "Wall"
+	},
+	{
+		"firstName": "Phoebe",
+		"lastName": "Pruitt"
+	},
+	{
+		"firstName": "Emily",
+		"lastName": "Kramer"
+	},
+	{
+		"firstName": "Colorado",
+		"lastName": "Landry"
+	},
+	{
+		"firstName": "Deborah",
+		"lastName": "Briggs"
+	},
+	{
+		"firstName": "Jin",
+		"lastName": "Richardson"
+	},
+	{
+		"firstName": "Dieter",
+		"lastName": "Chambers"
+	},
+	{
+		"firstName": "Ingrid",
+		"lastName": "Mathis"
+	},
+	{
+		"firstName": "Cara",
+		"lastName": "Spence"
+	},
+	{
+		"firstName": "Tate",
+		"lastName": "Webb"
+	},
+	{
+		"firstName": "Dieter",
+		"lastName": "Zimmerman"
+	},
+	{
+		"firstName": "Katelyn",
+		"lastName": "Hutchinson"
+	},
+	{
+		"firstName": "Armand",
+		"lastName": "Singleton"
+	},
+	{
+		"firstName": "Judah",
+		"lastName": "Kramer"
+	},
+	{
+		"firstName": "Colin",
+		"lastName": "Cortez"
+	},
+	{
+		"firstName": "Jenna",
+		"lastName": "Rosales"
+	},
+	{
+		"firstName": "Hunter",
+		"lastName": "Schmidt"
+	},
+	{
+		"firstName": "Renee",
+		"lastName": "Nielsen"
+	},
+	{
+		"firstName": "Joel",
+		"lastName": "Boyer"
+	},
+	{
+		"firstName": "Cheryl",
+		"lastName": "Pugh"
+	},
+	{
+		"firstName": "Callie",
+		"lastName": "Schroeder"
+	},
+	{
+		"firstName": "Alea",
+		"lastName": "Hendricks"
+	},
+	{
+		"firstName": "Myra",
+		"lastName": "Acevedo"
+	},
+	{
+		"firstName": "Alexandra",
+		"lastName": "Booker"
+	},
+	{
+		"firstName": "Kai",
+		"lastName": "Quinn"
+	},
+	{
+		"firstName": "Mannix",
+		"lastName": "Lamb"
+	},
+	{
+		"firstName": "Hadassah",
+		"lastName": "Floyd"
+	},
+	{
+		"firstName": "Neve",
+		"lastName": "Anderson"
+	},
+	{
+		"firstName": "Cadman",
+		"lastName": "Burke"
+	},
+	{
+		"firstName": "Maris",
+		"lastName": "Ruiz"
+	},
+	{
+		"firstName": "Alyssa",
+		"lastName": "Blake"
+	},
+	{
+		"firstName": "Norman",
+		"lastName": "Guerrero"
+	},
+	{
+		"firstName": "Leilani",
+		"lastName": "Britt"
+	},
+	{
+		"firstName": "Price",
+		"lastName": "Lloyd"
+	},
+	{
+		"firstName": "Ryan",
+		"lastName": "Bentley"
+	},
+	{
+		"firstName": "Robin",
+		"lastName": "Long"
+	},
+	{
+		"firstName": "Luke",
+		"lastName": "Powers"
+	},
+	{
+		"firstName": "Addison",
+		"lastName": "Edwards"
+	},
+	{
+		"firstName": "Yardley",
+		"lastName": "Vega"
+	},
+	{
+		"firstName": "Brent",
+		"lastName": "Lindsey"
+	},
+	{
+		"firstName": "Fitzgerald",
+		"lastName": "Burns"
+	},
+	{
+		"firstName": "Keefe",
+		"lastName": "Leonard"
+	},
+	{
+		"firstName": "Tashya",
+		"lastName": "Mayo"
+	},
+	{
+		"firstName": "Cally",
+		"lastName": "Mccullough"
+	},
+	{
+		"firstName": "Craig",
+		"lastName": "Gallagher"
+	},
+	{
+		"firstName": "Jada",
+		"lastName": "Leblanc"
+	},
+	{
+		"firstName": "Paul",
+		"lastName": "Dickson"
+	},
+	{
+		"firstName": "Cara",
+		"lastName": "Sims"
+	},
+	{
+		"firstName": "Bruno",
+		"lastName": "Baker"
+	},
+	{
+		"firstName": "Lionel",
+		"lastName": "Pearson"
+	},
+	{
+		"firstName": "Eve",
+		"lastName": "Schwartz"
+	},
+	{
+		"firstName": "Neve",
+		"lastName": "Beard"
+	},
+	{
+		"firstName": "Wang",
+		"lastName": "Leonard"
+	},
+	{
+		"firstName": "Cyrus",
+		"lastName": "Decker"
+	},
+	{
+		"firstName": "Demetrius",
+		"lastName": "Levy"
+	},
+	{
+		"firstName": "Cade",
+		"lastName": "Haynes"
+	},
+	{
+		"firstName": "Mason",
+		"lastName": "Schroeder"
+	},
+	{
+		"firstName": "Elmo",
+		"lastName": "Herrera"
+	},
+	{
+		"firstName": "Melodie",
+		"lastName": "Garza"
+	},
+	{
+		"firstName": "Kenneth",
+		"lastName": "Mccormick"
+	},
+	{
+		"firstName": "Galvin",
+		"lastName": "Barry"
+	},
+	{
+		"firstName": "Bryar",
+		"lastName": "Hodges"
+	},
+	{
+		"firstName": "Tate",
+		"lastName": "Madden"
+	},
+	{
+		"firstName": "Fulton",
+		"lastName": "Serrano"
+	},
+	{
+		"firstName": "Jenna",
+		"lastName": "Hopkins"
+	},
+	{
+		"firstName": "Angela",
+		"lastName": "Pruitt"
+	},
+	{
+		"firstName": "Troy",
+		"lastName": "Landry"
+	},
+	{
+		"firstName": "Elizabeth",
+		"lastName": "Wolfe"
+	},
+	{
+		"firstName": "Erica",
+		"lastName": "Mcbride"
+	},
+	{
+		"firstName": "Jesse",
+		"lastName": "Mcdonald"
+	},
+	{
+		"firstName": "Garrett",
+		"lastName": "Harrington"
+	},
+	{
+		"firstName": "Ralph",
+		"lastName": "Castillo"
+	},
+	{
+		"firstName": "Mikayla",
+		"lastName": "Britt"
+	},
+	{
+		"firstName": "Ivana",
+		"lastName": "Rowe"
+	},
+	{
+		"firstName": "Candice",
+		"lastName": "Sanders"
+	},
+	{
+		"firstName": "Charles",
+		"lastName": "Griffith"
+	},
+	{
+		"firstName": "Evan",
+		"lastName": "Hubbard"
+	},
+	{
+		"firstName": "Addison",
+		"lastName": "Bullock"
+	},
+	{
+		"firstName": "Keiko",
+		"lastName": "Shields"
+	},
+	{
+		"firstName": "Amir",
+		"lastName": "Bass"
+	},
+	{
+		"firstName": "Kenneth",
+		"lastName": "Leach"
+	}
+ ] // end dataList
+} // end export class myComponent
